3438d1a4d850c90d3dadd0785ff61868cb59cb99
Radosław Grzęda <radoslaw.grzeda@pjwstk.edu.pl>
2025-03-14 12:30:57 +0100

    Modyfikacja2

--- pracDom/pracDom/Program.cs
+++ pracDom/pracDom/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 for (int i = 0; i < 10; i++)
+    for (int j = 0; j < 10; j++)
 {
-    Console.Out.WriteLine(i);
+    Console.Out.Write(i);
+    Console.Out.Write(j);
+    
+    Console.Out.WriteLine();
 }
